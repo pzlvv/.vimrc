043f53721b970b7284d9a6bedcfb183c4f284cac
Zane <pz.pzlvv@gmail.com>
2017-07-27 22:18:08 +0800

    Bug fix

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors ~/.vim/backup ~/.vim/swap ~/.vim/undo
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim -k
 curl -LSso ~/.vim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -k
-git clone https://github.com/python-mode/python-mode.git ~/.vim/bundle/python-mode || cd ~/vim/bundle/python-mode; git pull
-git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go || cd ~/vim/bundle/vim-go; git pull
+git clone https://github.com/python-mode/python-mode.git ~/.vim/bundle/python-mode || cd ~/.vim/bundle/python-mode; git pull
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go || cd ~/.vim/bundle/vim-go; git pull
 curl -LSso ~/.vimrc https://raw.githubusercontent.com/pzlvv/vimrc/master/.vimrc -k
